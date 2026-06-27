eb5a15d3dfd1476b142fa20767a3b500e7134133
Grey_Turizo <carolinatudi@gmail.com>
2026-06-27 10:21:01 -0500

    Agregar anotaciones Jackson a UserDto
    - Agregar @JsonProperty explícito a todos los campos
    - Asegurar serialización correcta de UserDto a JSON
    - Solucionar problema de campos vacíos en respuesta

--- src/main/java/co/edu/sena/productsreact/dto/auth/UserDto.java
+++ src/main/java/co/edu/sena/productsreact/dto/auth/UserDto.java
@@ -1,8 +1,17 @@
 package co.edu.sena.productsreact.dto.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record UserDto(
+        @JsonProperty("username")
         String username,
+
+        @JsonProperty("email")
         String email,
+
+        @JsonProperty("role")
         String role,
+
+        @JsonProperty("avatarUrl")
         String avatarUrl
 ) {}
